d6986f3d154dd9fe0d83c9072228bfa80091e261
sbin0819 <sbinha123@gmail.com>
2021-03-02 22:26:46 +0900

    some changed

---
+++ src/index.js
@@ -0,0 +1,12 @@
+class Organization {
+  constructor(data) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+  get name() {
+    return this._name;
+  }
+  get country() {
+    return this._country;
+  }
+}
